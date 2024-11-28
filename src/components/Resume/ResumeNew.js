7245e0807043900edb5a1dbc77a227a9f85e502d
Anisha1903 <anisham0219@gmail.com>
2024-11-28 13:32:24 +0530

    first commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,3 +1,8 @@
+
+// For multi page resume
+
+/*
+
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
@@ -17,11 +22,11 @@ function ResumeNew() {
   }, []);
 
   const nextPage = () => {
-    setPageNumber(prevPageNumber => prevPageNumber + 1);
+   setPageNumber(prevPageNumber => prevPageNumber + 1);
   };
 
   const prevPage = () => {
-    setPageNumber(prevPageNumber => prevPageNumber - 1);
+   setPageNumber(prevPageNumber => prevPageNumber - 1);
   };
 
   return (
@@ -67,6 +72,59 @@ function ResumeNew() {
           >
             Next
           </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
+
+*/
+
+
+// for single page resume
+
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/../Assets/Anisha-M-CV.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        <Row className="resume">
+          <Col className="d-flex justify-content-center">
+            <Document file={pdf}>
+              <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            </Document>
+          </Col>
         </Row>
       </Container>
     </div>
